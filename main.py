78469a97e2018500287bc81a62aafc8b32de9420
Aaron Snoswell <a.snoswell@qut.edu.au>
2021-08-08 18:44:47 +1000

    Add splash screen!

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
 #!/usr/bin/env python3
-import copy
 import traceback
 
 import tcod
 
 import color
 import exceptions
-from engine import Engine
-import entity_factories
 import exceptions
 import input_handlers
-from procgen import generate_dungeon
+import setup_game
 
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
-
-    map_width = 80
-    map_height = 43
-
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
-
-    max_monsters_per_room = 2
-    max_items_per_room = 2
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png",
@@ -34,27 +21,8 @@ def main() -> None:
         tcod.tileset.CHARMAP_TCOD
     )
 
-    player = copy.deepcopy(entity_factories.player)
-    engine = Engine(player=player)
-    engine.game_map = generate_dungeon(
-        max_rooms=max_rooms,
-        room_min_size=room_min_size,
-        room_max_size=room_max_size,
-        map_width=map_width,
-        map_height=map_height,
-        max_monsters_per_room=max_monsters_per_room,
-        max_items_per_room=max_items_per_room,
-        engine=engine
-    )
-    engine.update_fov()
-
-    engine.message_log.add_message(
-        "Hello and welcome, adventurer, to yet another dungeon!",
-        color.welcome_text
-    )
-
-    handler: input_handlers.BaseEventHandler = input_handlers.MainGameEventHandler(engine)
-
+    handler: input_handlers.BaseEventHandler = setup_game.MainMenu()
+    
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
